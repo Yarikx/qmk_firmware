020e8626370290ee87e5acba9c1d05d204d9fb1e
Yaroslav Heriatovych <Yaraikx@gmail.com>
2020-03-11 09:41:47 +0200

    basic support for nrfmicro lily58

--- keyboards/lily58_ble/config.h
+++ keyboards/lily58_ble/config.h
@@ -21,8 +21,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define BLE_NUS_MIN_INTERVAL 30
-#define BLE_NUS_MAX_INTERVAL 70
+// #define BLE_NUS_MIN_INTERVAL 30
+// #define BLE_NUS_MAX_INTERVAL 70
+
+#define BLE_NUS_MIN_INTERVAL 30 // default 60
+#define BLE_NUS_MAX_INTERVAL 30 // default 75
+#define BLE_HID_MAX_INTERVAL 50 // default 90
+#define BLE_HID_SLAVE_LATENCY 2 // default 4
+#define ENABLE_STARTUP_ADV_NOLIST
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFC51
--- keyboards/lily58_ble/keymaps/default/keymap.c
+++ keyboards/lily58_ble/keymaps/default/keymap.c
@@ -63,6 +63,8 @@ enum layer_number {
 #define XXXXXXX KC_NO
 
 
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
@@ -81,17 +83,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, \
-                       MO(_LOWER),KC_LGUI, KC_LALT, LT(_LOWER,KC_SPC),  LT(_RAISE,KC_ENT),  KC_BSPC, KC_RGUI, MO(_RAISE) \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______, LGUI(KC_SPC),KC_N,KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                       KC_LALT,KC_LGUI, MO(_LOWER), KC_SPC,   KC_ENT,  MO(_RAISE), KC_RALT, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |AD_WOL|ADVID1|ADVID2|ADVID3|ADVID4|      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |ENTSLP|ENTDFU|                    |      |      |      |      |      |      |
+ * |      |      |      |      |ENTSLP|ENTDFU|                    |      |      |      |      |      |      |                             
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |USBDIS|USB_EN|-------.    ,-------|   ~  |   !  |   @  |   #  |   $  |   %  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
@@ -102,10 +104,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `-------------------''-------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  AD_WO_L, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4, XXXXXXX,                  _______, _______, _______,  _______, _______,  _______,\
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ENT_SLP, ENT_DFU,                  _______, _______, _______,  _______, _______,  _______, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, USB_DIS, USB_EN,                   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, \
-  BATT_LV, DEL_ID1, XXXXXXX, XXXXXXX, BLE_DIS, BLE_EN, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
+  AD_WO_L, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4, DELBNDS,                  _______, _______, _______, _______, _______, _______,\
+  DELBNDS, DEL_ID1, DEL_ID2, DEL_ID3, ENT_SLP, ENT_DFU,                  KC_GRV,  KC_PGUP, KC_UP,   KC_PGDN, KC_RBRC, KC_BSLS, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, USB_DIS, USB_EN,                   KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BLE_DIS, BLE_EN, _______, _______, KC_RCBR, KC_HOME, KC_END,  _______, _______, _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
@@ -125,9 +127,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT( \
   KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, _______, _______,  _______, _______,  _______,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
-  _______, _______, _______,  _______, _______,  _______, _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  _______, _______, _______,  _______, _______,  KC_MINS,                   KC_PLUS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
+  _______, _______, _______,  _______, _______,  KC_UNDS, _______, _______, KC_EQL , KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
                               _______, _______, _______,  _______, _______,  _______, _______, _______ \
 )
 };
@@ -162,6 +164,7 @@ void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
   }
 }
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  NRF_LOG_INFO("Process key");
   char str[16];
  
   if (record->event.pressed) {
@@ -234,3 +237,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 ;
+
+// void matrix_init_user() {
+// //  rgblight_mode_noeeprom(35);
+//   // set_usb_enabled(true);
+//   // NRF_LOG_INFO("Hello");
+// }
